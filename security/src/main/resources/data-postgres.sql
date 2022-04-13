09bcc9750d61f0df14c74e7ff263963bbd36ef96
djordjekrsmanovic <73399785+djordjekrsmanovic@users.noreply.github.com>
2022-04-13 19:41:03 +0200

    feat:adding pasword hashing

--- security/src/main/resources/data-postgres.sql
+++ security/src/main/resources/data-postgres.sql
@@ -3,39 +3,39 @@
 
 INSERT INTO public.client
 (id, common_name, country, given_name, mail, organization, organization_unit, password, role, surname) VALUES
-(1,'Admin Admin', 'Serbia', 'Admin', 'admin@gmail.com', 'PKI', 'PKI-Novi Sad', '123', 0, 'Admin');
+(1,'Admin Admin', 'Serbia', 'Admin', 'admin@gmail.com', 'PKI', 'PKI-Novi Sad', '$2a$10$KqyJ6IcfwIwV4pkjbfQ7LeGlhRRHTlIKtPKS7ElLJ0NcsOP2Zgpqm', 0, 'Admin');
 
 INSERT INTO public.client
 (id, common_name, country, given_name, mail, organization, organization_unit, password, role, surname) VALUES
-(2,'Rastislav Kukucka', 'Serbia', 'Rasti', 'rastislav@gmail.com', 'PKI', 'PKI-Novi Sad', '123', 0, 'Rasti');
+(2,'Rastislav Kukucka', 'Serbia', 'Rasti', 'rastislav@gmail.com', 'PKI', 'PKI-Novi Sad', '$2a$10$KqyJ6IcfwIwV4pkjbfQ7LeGlhRRHTlIKtPKS7ElLJ0NcsOP2Zgpqm', 0, 'Rasti');
 
 INSERT INTO public.client
 (id, common_name, country, given_name, mail, organization, organization_unit, password, role, surname) VALUES
-(3,'Djordje Krsmanovic', 'Serbia', 'Djordje', 'djordje@gmail.com', 'FTN', 'FTN-Novi Sad', '123', 0, 'Djordje');
+(3,'Djordje Krsmanovic', 'Serbia', 'Djordje', 'djordje@gmail.com', 'FTN', 'FTN-Novi Sad', '$2a$10$KqyJ6IcfwIwV4pkjbfQ7LeGlhRRHTlIKtPKS7ElLJ0NcsOP2Zgpqm', 0, 'Djordje');
 
 INSERT INTO public.client
 (id, common_name, country, given_name, mail, organization, organization_unit, password, role, surname) VALUES
-(4,'Bojan Prodanovic', 'Serbia', 'Bojan', 'bojan@gmail.com', 'FTN', 'FTN-Novi Sad', '123', 0, 'Bojan');
+(4,'Bojan Prodanovic', 'Serbia', 'Bojan', 'bojan@gmail.com', 'FTN', 'FTN-Novi Sad', '$2a$10$KqyJ6IcfwIwV4pkjbfQ7LeGlhRRHTlIKtPKS7ElLJ0NcsOP2Zgpqm', 0, 'Bojan');
 
 INSERT INTO public.client
 (id, common_name, country, given_name, mail, organization, organization_unit, password, role, surname) VALUES
-(5,'Aleksandar Savic', 'Serbia', 'Aleksandar', 'aleksandar@gmail.com', 'FTN', 'FTN-Novi Sad', '123', 0, 'Aleksandar');
+(5,'Aleksandar Savic', 'Serbia', 'Aleksandar', 'aleksandar@gmail.com', 'FTN', 'FTN-Novi Sad', '$2a$10$KqyJ6IcfwIwV4pkjbfQ7LeGlhRRHTlIKtPKS7ElLJ0NcsOP2Zgpqm', 0, 'Aleksandar');
 
 
 -- USERS
 
 INSERT INTO public.client
 (id, common_name, country, given_name, mail, organization, organization_unit, password, role, surname) VALUES
-(6,'User User', 'Serbia', 'NiceUser', 'user@gmail.com', 'PKI', 'PKI-Novi Sad', '123', 1, 'User');
+(6,'User User', 'Serbia', 'NiceUser', 'user@gmail.com', 'PKI', 'PKI-Novi Sad', '$2a$10$KqyJ6IcfwIwV4pkjbfQ7LeGlhRRHTlIKtPKS7ElLJ0NcsOP2Zgpqm', 1, 'User');
 
 INSERT INTO public.client
 (id, common_name, country, given_name, mail, organization, organization_unit, password, role, surname) VALUES
-(7,'Pavle Stefanovic', 'Bosnia', 'pavle', 'pavle@gmail.com', 'Continental', 'Continental-Novi Sad', '123', 1, 'Pavle');
+(7,'Pavle Stefanovic', 'Bosnia', 'pavle', 'pavle@gmail.com', 'Continental', 'Continental-Novi Sad', '$2a$10$KqyJ6IcfwIwV4pkjbfQ7LeGlhRRHTlIKtPKS7ElLJ0NcsOP2Zgpqm', 1, 'Pavle');
 
 INSERT INTO public.client
 (id, common_name, country, given_name, mail, organization, organization_unit, password, role, surname) VALUES
-(8,'Marko Njegosov', 'Serbia', 'Marko', 'marko@gmail.com', 'SuperORG', 'SORG-Beograd', '123', 1, 'Marko');
+(8,'Marko Njegosov', 'Serbia', 'Marko', 'marko@gmail.com', 'SuperORG', 'SORG-Beograd', '$2a$10$KqyJ6IcfwIwV4pkjbfQ7LeGlhRRHTlIKtPKS7ElLJ0NcsOP2Zgpqm', 1, 'Marko');
 
 INSERT INTO public.client
 (id, common_name, country, given_name, mail, organization, organization_unit, password, role, surname) VALUES
-(9,'XWS project', 'Serbia', 'xws', 'dislinkt@gmail.com', 'DislinktORG', 'XWS-ftn', '123', 1, 'dislinkt');
+(9,'XWS project', 'Serbia', 'xws', 'dislinkt@gmail.com', 'DislinktORG', 'XWS-ftn', '$2a$10$KqyJ6IcfwIwV4pkjbfQ7LeGlhRRHTlIKtPKS7ElLJ0NcsOP2Zgpqm', 1, 'dislinkt');
